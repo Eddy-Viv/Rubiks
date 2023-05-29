aab22a52a3bf6fbfa118e20886fe56dc6dc6122d
Ed Vivian <ejovivian@hotmail.com>
2023-05-30 01:02:06 +0930

    partway through implementing cube class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
 namespace Rubiks
 {
+
+  internal class CubePuzzle { 
+    private int [,] faces = new int [6,8];
+  
+    public CubePuzzle(){ 
+      init();
+    }
+
+    public void init(){ 
+      for (int i = 0; i < 6; i++){ 
+        for (int j = 0; j < 8; j++){ 
+          faces [i, j] = i;
+        }
+      }
+    }
+  }
+
   internal static class Program
   {
 
